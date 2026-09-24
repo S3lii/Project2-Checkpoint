42033bd3c5853fe4267bb8c2be23987e2291830f
Sayed Ali <mosawiamam99@gmail.com>
2026-09-24 16:02:32 +0300

    Make Review  Model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,18 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 const userSchema = new mongoose.Schema({
   username: {
-    type: String,
-    required: true,
-    unique: true,
+    type: String , 
+    required: true , 
+    unique: true , 
     trim: true
-  },
+  } , 
   password: {
-    type: String,
-    required: true,
-  },
-}, {timestamps: true});
+    type: String , 
+    required: true
+  }
+} , { timestamps: true })
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User" , userSchema)
 
-module.exports = User;
+module.exports = User
